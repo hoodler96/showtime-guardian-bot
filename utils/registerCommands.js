dd80358b83a3cbb54af8b4d8c05441f639f3464c
hoodler96 <mwinvestmentsolutionsinc@gmail.com>
2026-08-07 21:17:09 -0400

    Update registerCommands.js

--- utils/registerCommands.js
+++ utils/registerCommands.js
@@ -59,7 +59,83 @@ module.exports = async function registerCommands(clientId, guildId, botToken) {
 
     new SlashCommandBuilder()
       .setName('reports')
-      .setDescription('View recent reports')
+      .setDescription('View recent reports'),
+
+    new SlashCommandBuilder()
+      .setName('bouncer')
+      .setDescription('Staff moderation controls')
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('status')
+          .setDescription('View moderation status for a member')
+          .addUserOption(option =>
+            option
+              .setName('user')
+              .setDescription('Member to review')
+              .setRequired(true)
+          )
+      )
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('strikes')
+          .setDescription('View a member\'s current strike count')
+          .addUserOption(option =>
+            option
+              .setName('user')
+              .setDescription('Member to check')
+              .setRequired(true)
+          )
+      )
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('remove-strike')
+          .setDescription('Remove one strike from a member')
+          .addUserOption(option =>
+            option
+              .setName('user')
+              .setDescription('Member to remove one strike from')
+              .setRequired(true)
+          )
+      )
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('clear-strikes')
+          .setDescription('Clear all strikes from a member')
+          .addUserOption(option =>
+            option
+              .setName('user')
+              .setDescription('Member whose strikes should be cleared')
+              .setRequired(true)
+          )
+      )
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('untimeout')
+          .setDescription('Immediately remove a member timeout')
+          .addUserOption(option =>
+            option
+              .setName('user')
+              .setDescription('Member whose timeout should be removed')
+              .setRequired(true)
+          )
+      )
+
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('unban')
+          .setDescription('Remove a ban using the Discord user ID')
+          .addStringOption(option =>
+            option
+              .setName('user_id')
+              .setDescription('Discord user ID of the banned member')
+              .setRequired(true)
+          )
+      )
   ].map(command => command.toJSON());
 
   const rest = new REST({ version: '10' }).setToken(cleanToken);
